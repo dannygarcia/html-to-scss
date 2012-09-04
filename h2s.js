252c00e0ad0866bdcc5a36301b88bc49f9fba98f
Danny Garcia <danny.d.g@gmail.com>
2012-09-04 00:23:02 -0700

    whitespace and removing old stuff

--- h2s.js
+++ h2s.js
@@ -157,87 +157,12 @@ var Parser = function (options) {
 var h2s = new Parser(options);
 
 if (typeof options.html.val === 'undefined' || argv.help || argv.h) {
+
 	h2s.showHelp();
 	return;
+
+} else {
+
+	h2s.init();
+
 }
-
-h2s.init();
-
-/*
-
-var showHelp = function () {
-	console.log("F");
-};
-
-var crawl = function ($el) {
-
-	var i = 0,
-		j = 0,
-		tabs = '',
-		level = $el.parents().length,
-		$children = $el.children(),
-		className = $el.attr('class'),
-		classes = className.length ? className.split(' ') : 0,
-		name = $el[0].nodeName.toLowerCase();
-
-	for (i = 0; i < level; i++) {
-		tabs += tab;
-	}
-
-	if (classes !== 0) {
-		for (j = 0; j < classes.length; j++) {
-			name += '.' + classes[j];
-		}
-	}
-
-	if (name !== prevName) {
-
-		output += tabs + name + ' { \n\n';
-		console.log(tabs + name + ' { ');
-		console.log('');
-
-		if ($children.length) {
-
-			$children.each(function () {
-				crawl($(this));
-			});
-
-		}
-
-		output += tabs + '}\n';
-		console.log(tabs + '}');
-	}
-
-	prevName = name;
-
-
-};
-
-jsdom.env({
-	html: html,
-	scripts: [
-		'http://code.jquery.com/jquery-1.8.0.min.js'
-	],
-	done: function(errors, window) {
-		console.log(errors);
-		$ = window.$;
-		console.log('Parsing DOM');
-		crawl($('html'));
-		console.log("Done Parsing DOM");
-		if (typeof toFile !== 'undefined') {
-
-			console.log("Saving to file...");
-
-			fs.writeFile(toFile, output, function(err) {
-				if(err) {
-					console.log(err);
-				} else {
-					console.log("Output saved to " + toFile);
-				}
-			});
-
-		}
-	}
-});
-
-*/
